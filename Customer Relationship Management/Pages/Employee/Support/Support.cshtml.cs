7893c0aafe412b5cd418b4bf632bf2a5c2df50c7
Dân An <trandangdananime@gmail.com>
2025-11-13 12:35:26 +0700

    Cập nhật UI lại cho Employee/Support

---
+++ Customer Relationship Management/Pages/Employee/Support/Support.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Customer_Relationship_Management.Pages.Employee.Support
+{
+    public class SupportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
